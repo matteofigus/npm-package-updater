b28223290a47cbab2dcb29d2842b90183b5b022a
Arnold Zokas <arnold.zokas@coderoom.net>
2016-09-22 16:16:48 +0100

    add githubService.getFileContents

---
+++ tests/services/githubService.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const expect        = require('expect.js');
+const githubService = require('../../src/services/githubService');
+
+describe('githubService', () => {
+    describe('getFileContent', () => {
+        describe('given options and file path', () => {
+            it('should return file', (next) => {
+                const options = { apiToken: process.env.TOKEN, repo: { user: 'opentable', name: 'npm-package-updater' } };
+                const path = '/package.json';
+
+                githubService.getFileContent(options, path, (err, file) => {
+                    expect(file.name).to.be('npm-package-updater');
+                    next();
+                });
+            });
+        });
+
+        describe('given options and non-existent file path', () => {
+            it('should return error', (next) => {
+                const options = { apiToken: process.env.TOKEN, repo: { user: 'opentable', name: 'npm-package-updater' } };
+                const path = '/its.a.trap';
+
+                githubService.getFileContent(options, path, (err) => {
+                    expect(err.status).to.be('Not Found');
+                    next();
+                });
+            });
+        });
+    });
+});
