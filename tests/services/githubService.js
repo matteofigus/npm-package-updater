632d2b27fa39b7b7bda4b4919aa23d4758ec0164
Arnold Zokas <arnold.zokas@coderoom.net>
2016-09-22 17:04:58 +0100

    make githubService test execution conditional

--- tests/services/githubService.js
+++ tests/services/githubService.js
@@ -3,30 +3,34 @@
 const expect        = require('expect.js');
 const githubService = require('../../src/services/githubService');
 
-describe('githubService', () => {
-    describe('getFileContent', () => {
-        describe('given options and file path', () => {
-            it('should return file', (next) => {
-                const options = { apiToken: process.env.TOKEN, repo: { user: 'opentable', name: 'npm-package-updater' } };
-                const path = '/package.json';
+if (!process.env.TOKEN) {
+    console.log('Github auth token not defined. Skipping "githubService" tests...');
+} else {
+    describe('githubService', () => {
+        describe('getFileContent', () => {
+            describe('given options and file path', () => {
+                it('should return file', (next) => {
+                    const options = { apiToken: process.env.TOKEN, repo: { user: 'opentable', name: 'npm-package-updater' } };
+                    const path = '/package.json';
 
-                githubService.getFileContent(options, path, (err, file) => {
-                    expect(file.name).to.be('npm-package-updater');
-                    next();
+                    githubService.getFileContent(options, path, (err, file) => {
+                        expect(file.name).to.be('npm-package-updater');
+                        next();
+                    });
                 });
             });
-        });
 
-        describe('given options and non-existent file path', () => {
-            it('should return error', (next) => {
-                const options = { apiToken: process.env.TOKEN, repo: { user: 'opentable', name: 'npm-package-updater' } };
-                const path = '/its.a.trap';
+            describe('given options and non-existent file path', () => {
+                it('should return error', (next) => {
+                    const options = { apiToken: process.env.TOKEN, repo: { user: 'opentable', name: 'npm-package-updater' } };
+                    const path = '/its.a.trap';
 
-                githubService.getFileContent(options, path, (err) => {
-                    expect(err.status).to.be('Not Found');
-                    next();
+                    githubService.getFileContent(options, path, (err) => {
+                        expect(err.status).to.be('Not Found');
+                        next();
+                    });
                 });
             });
         });
     });
-});
+}
